23aeeaa1af9e7ceca36f46170edd40126a9884c5
kaan kara <kaankara3801@hotmail.com>
2021-01-20 16:03:58 +0300

    20/01/2021

---
+++ Metotlar/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string urunAdi = ("Elma");
+            double fiyati = 15;
+            string aciklama = ("Amasya Elmasi");
+
+            string[] meyveler = new string[] { };
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elmasi";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakir Karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1, urun2 };
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("-----------------");
+
+            }
+
+            Console.WriteLine("--------------Metotlar------------------");
+
+            //instance - ornek
+            //encapsulation
+            SepetManager sepetManager = new SepetManager();
+
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yesil Armut", 12, 10);
+            sepetManager.Ekle2("Elma", "Yesil Elma", 12,9 );
+            sepetManager.Ekle2("Armut", "Diyarbakir karpuzu", 12, 8);
+        }
+    }
+}
+
+
+//dont repeat yoursself - DRY - Clean Code - Best Practice
